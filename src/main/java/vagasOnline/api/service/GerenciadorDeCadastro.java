12de1c902c7ae8e4a3d6ddb8b7bb5e9fc05a1924
luis-fbs <luis-fbs@outlook.com>
2024-06-08 19:17:05 -0300

    Gerencia cadastros

---
+++ src/main/java/vagasOnline/api/service/GerenciadorDeCadastro.java
@@ -0,0 +1,55 @@
+package vagasOnline.api.service;
+
+import org.springframework.stereotype.Service;
+import vagasOnline.api.usuario.Empresa;
+import vagasOnline.api.usuario.Pessoa;
+import vagasOnline.api.usuario.Usuario;
+import vagasOnline.api.vaga.Vaga;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GerenciadorDeCadastro {
+    private List<Empresa> empresasCadastradas;
+    private List<Pessoa> pessoasCadastradas;
+    private List<Vaga> vagasCadastradas;
+
+    public GerenciadorDeCadastro() {
+        this.empresasCadastradas = new ArrayList<Empresa>();
+        this.pessoasCadastradas = new ArrayList<Pessoa>();
+        this.vagasCadastradas = new ArrayList<Vaga>();
+    }
+
+    public void cadastrarUsuario(Usuario usuario){
+        if (usuario instanceof Empresa){
+            this.empresasCadastradas.add((Empresa) usuario);
+        } else if (usuario instanceof Pessoa) {
+            this.pessoasCadastradas.add((Pessoa) usuario);
+        }
+    }
+
+    public void excluirUsuario(Usuario usuario){
+        if (usuario instanceof Empresa){
+            this.empresasCadastradas.remove((Empresa) usuario);
+        } else if (usuario instanceof Pessoa) {
+            this.pessoasCadastradas.remove((Pessoa) usuario);
+        }
+    }
+
+    public void cadastrarVaga(Vaga vaga){
+        this.vagasCadastradas.add(vaga);
+    }
+
+    public void excluirVaga(Vaga vaga){
+        this.vagasCadastradas.remove(vaga);
+    }
+
+    public boolean usuarioExiste(Usuario usuario){
+        if (usuario instanceof Empresa){
+            return this.empresasCadastradas.contains(usuario);
+        } else if (usuario instanceof Pessoa) {
+            return this.pessoasCadastradas.contains(usuario);
+        } else return false;
+    }
+}
