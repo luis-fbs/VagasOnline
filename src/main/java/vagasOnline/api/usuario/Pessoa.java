ba9484df0d3027dce1cf92958e53bdb74a6fb6b7
luis-fbs <luis-fbs@outlook.com>
2024-06-08 19:16:09 -0300

    Usuario Pessoa

---
+++ src/main/java/vagasOnline/api/usuario/Pessoa.java
@@ -0,0 +1,32 @@
+package vagasOnline.api.usuario;
+
+import vagasOnline.api.vaga.Vaga;
+
+import java.util.ArrayList;
+
+public class Pessoa extends Usuario{
+    private String cpf, curriculo;
+
+    public Pessoa(String foto, String nome, String senha, String cpf, String curriculo) {
+        super(foto, nome, senha);
+        this.cpf = cpf;
+        this.curriculo = curriculo;
+    }
+
+    public String getCurriculo() {
+        return curriculo;
+    }
+
+    public void setCurriculo(String curriculo) {
+        this.curriculo = curriculo;
+    }
+
+    public void cadastarEmVaga(Vaga vaga){
+        this.adiconarVagaNaListaDeCadastradas(vaga);
+    }
+
+    public void desistirDaVaga(Vaga vaga){
+        this.excluiVagaDaListaDeCadastradas(vaga);
+    }
+
+}
