02e9c3a17a02f69edfc221b32b796de90f305696
luis-fbs <luis-fbs@outlook.com>
2024-06-08 19:13:08 -0300

    Principal

---
+++ src/main/java/vagasOnline/api/ApiApplication.java
@@ -0,0 +1,11 @@
+package vagasOnline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
