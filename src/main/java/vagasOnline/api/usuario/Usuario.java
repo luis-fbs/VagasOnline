23470b098a29ef910cd3b51347f91d40b258c11b
luis-fbs <luis-fbs@outlook.com>
2024-06-08 19:15:15 -0300

    Usuario

---
+++ src/main/java/vagasOnline/api/usuario/Usuario.java
@@ -0,0 +1,46 @@
+package vagasOnline.api.usuario;
+
+import vagasOnline.api.vaga.Vaga;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Usuario {
+    private String foto, nome, senha;
+    private List<Vaga> vagasCadastradas;
+
+    public Usuario(String foto, String nome, String senha) {
+        this.foto = foto;
+        this.nome = nome;
+        this.senha = senha;
+        this.vagasCadastradas = new ArrayList<Vaga>();
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Vaga> getVagasCadastradas() {
+        return vagasCadastradas;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void adiconarVagaNaListaDeCadastradas(Vaga vaga){
+        this.vagasCadastradas.add(vaga);
+    }
+
+    public void excluiVagaDaListaDeCadastradas(Vaga vaga){
+        this.vagasCadastradas.remove(vaga);
+    }
+}
