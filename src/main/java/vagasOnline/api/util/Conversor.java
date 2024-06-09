92f24ef3ac314e215b67283b5b23d838b81317a4
luis-fbs <luis-fbs@outlook.com>
2024-06-09 15:55:59 -0300

    Função para converter de json para Usuario

--- src/main/java/vagasOnline/api/util/Conversor.java
+++ src/main/java/vagasOnline/api/util/Conversor.java
@@ -1,6 +1,21 @@
 package vagasOnline.api.util;
 
-public class Conversor {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import vagasOnline.api.usuario.Empresa;
+import vagasOnline.api.usuario.Pessoa;
+import vagasOnline.api.usuario.Usuario;
 
+public class Conversor {
+    public static Usuario jsonParaUsuario(String json){
+        Gson gson = new Gson();
+        JsonObject parser = gson.fromJson(json, JsonObject.class);
+
+        if (parser.has("cpf")){
+            return gson.fromJson(json, Pessoa.class);
+        } else if (parser.has("cnpj")) {
+            return gson.fromJson(json, Empresa.class);
+        } else return null;
+    }
 
 }
