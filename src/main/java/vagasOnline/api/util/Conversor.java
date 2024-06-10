83b138bd818e87a74dcd29e1d4a993ec93b96795
luis-fbs <baptistabaptista@discente.ufg.br>
2024-06-10 14:51:44 -0300

    Trata caso de sintaxe de json invalida

--- src/main/java/vagasOnline/api/util/Conversor.java
+++ src/main/java/vagasOnline/api/util/Conversor.java
@@ -2,6 +2,7 @@ package vagasOnline.api.util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import vagasOnline.api.usuario.Empresa;
 import vagasOnline.api.usuario.Pessoa;
 import vagasOnline.api.usuario.Usuario;
@@ -9,13 +10,17 @@ import vagasOnline.api.usuario.Usuario;
 public class Conversor {
     public static Usuario jsonParaUsuario(String json){
         Gson gson = new Gson();
-        JsonObject parser = gson.fromJson(json, JsonObject.class);
 
-        if (parser.has("cpf")){
-            return gson.fromJson(json, Pessoa.class);
-        } else if (parser.has("cnpj")) {
-            return gson.fromJson(json, Empresa.class);
-        } else return null;
+        try {
+            JsonObject parser = gson.fromJson(json, JsonObject.class);
+            if (parser.has("cpf")){
+                return gson.fromJson(json, Pessoa.class);
+            } else if (parser.has("cnpj")) {
+                return gson.fromJson(json, Empresa.class);
+            } else return null;
+        } catch (JsonSyntaxException exception){
+            System.out.println("Sintaxe invalida\nErro: " + exception);
+            return null;
+        }
     }
-
 }
