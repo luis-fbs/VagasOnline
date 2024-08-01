73089b7a8212aa8e4182251df1e1236075c51b87
luis-fbs <luis-fbs@outlook.com>
2024-08-01 16:35:51 -0300

    Condições para igualdade de vagas

--- src/main/java/vagasOnline/api/vaga/Vaga.java
+++ src/main/java/vagasOnline/api/vaga/Vaga.java
@@ -3,6 +3,7 @@ package vagasOnline.api.vaga;
 import vagasOnline.api.usuario.Empresa;
 import vagasOnline.api.usuario.Pessoa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vaga {
@@ -14,6 +15,7 @@ public class Vaga {
         this.cargo = cargo;
         this.descricao = descricao;
         this.empresa = empresa;
+        this.candidatos = new ArrayList<Pessoa>();
     }
 
     public void adicionarCandidato(Pessoa candidato){
@@ -22,5 +24,13 @@ public class Vaga {
 
     public void excluirCandidato(Pessoa candidato){
         this.candidatos.remove(candidato);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Vaga) &&
+                ((Vaga) obj).cargo.equals(this.cargo) &&
+                ((Vaga) obj).empresa.equals(this.empresa) &&
+                ((Vaga) obj).descricao.equals(this.descricao);
     }
 }
