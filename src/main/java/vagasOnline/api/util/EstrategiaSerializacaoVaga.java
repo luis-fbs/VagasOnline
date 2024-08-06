0173b9f571379595c5e63cb279d6eaf43bf4f0ab
luis-fbs <luis-fbs@outlook.com>
2024-08-06 18:27:59 -0300

    Define estratégia de serialização de vaga

---
+++ src/main/java/vagasOnline/api/util/EstrategiaSerializacaoVaga.java
@@ -0,0 +1,16 @@
+package vagasOnline.api.util;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+
+public class EstrategiaSerializacaoVaga implements ExclusionStrategy {
+    @Override
+    public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+        return "empresa".equals(fieldAttributes.getName());
+    }
+
+    @Override
+    public boolean shouldSkipClass(Class<?> aClass) {
+        return false;
+    }
+}
