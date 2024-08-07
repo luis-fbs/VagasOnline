9b83f7eab49a5a9b14e2a7c3ef635bdc0288b655
luis-fbs <luis-fbs@outlook.com>
2024-08-07 19:56:56 -0300

    CORS config

---
+++ src/main/java/vagasOnline/api/configuracoes/corsConfig.java
@@ -0,0 +1,22 @@
+package vagasOnline.api.configuracoes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfiguration() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
